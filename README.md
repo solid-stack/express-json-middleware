85924de84b50c7a8f0ad583c55c1decf304294c3
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-06-30 23:00:40 -0700

    adding name middleware stacks

--- README.md
+++ README.md
@@ -39,9 +39,23 @@ Your routes array or routes.json should be an array of arrays. Each line has 3 i
 1. The second item is the routes that this should apply to, `null` if it should be set directly on the default router with no routes.
 1. The third item is the array of middlewares to be run. Each string will be required in with the middlewares base prefixed to it.
 
+Additionally you can create named stacks of middleware using the "stack" word for the first item, a name for the second
+item, and a list of middlewares for the thired. These have to be 
+included before being applied. You can include the named stack in your later lists of middlewares and it will be expanded
+to the full list. This is handy if you have long lists of repeated middlewares.
+
+```json
+[
+    [ "stack",  "addPageParts", ["addHeader", "addFotter"]],
+    [ "GET",    "/",            ["addPageParts"]]
+]
+```
+
+
 ### routes.json
 
-```
+
+```json
 [
   [ "use",      "/",                                ["static"]],
   [ "use",      "/api/v1",                          ["grasshopper"]],
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var path = require('path');
+var path = require('path'),
+    _ = require('lodash');
 
 module.exports = routes;
 
 function routes(options) {
-    var app, express, routes, middlewares;
+    var app, express, routes, middlewares, stacks = {};
 
     if (!options) {
         throw new Error('No options object supplied.');
@@ -22,33 +23,47 @@ function routes(options) {
         var router;
 
         switch (route[0]) {
+        case 'stack':
+            stacks[route[1]] = route[2];
+            break;
             // passing in a router or middleware that doesn't need verb
-            case 'use':
-                router = getMiddlewares(route[2], middlewareBase);
-                if (!!route[1]) {
-                    app.use(route[1], router);
-                } else {
-                    app.use(router);
-                }
-                break;
+        case 'use':
+            router = getMiddlewares(route[2], middlewareBase, stacks);
+            if (!!route[1]) {
+                app.use(route[1], router);
+            } else {
+                app.use(router);
+            }
+            break;
             // Passing in middlewares
-            default:
-                router = express.Router();
-                router
-                    .route(route[1])
-                    [route[0].toLowerCase()](
-                    getMiddlewares(route[2], middlewareBase)
-                );
-                app.use('/', router);
-                break;
+        default:
+            router = express.Router();
+            router
+                .route(route[1])
+                [route[0].toLowerCase()](
+                getMiddlewares(route[2], middlewareBase, stacks)
+            );
+            app.use('/', router);
+            break;
         }
 
 
     });
 }
 
-function getMiddlewares(middlewares, middlewareBase) {
-    return middlewares.map(function(ware) {
-        return require(path.join(middlewareBase, ware));
-    });
+function getMiddlewares(middlewares, middlewareBase, stacks) {
+    return _
+        .chain(middlewares)
+        .map(function(ware) {
+            if (stacks[ware]) {
+                return stacks[ware];
+            } else {
+                return ware;
+            }
+        })
+        .flatten()
+        .map(function(ware) {
+            return require(path.join(middlewareBase, ware));
+        })
+        .value();
 }
